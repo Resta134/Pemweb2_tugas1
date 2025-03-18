36252ef044630e5d935424b489fe7357a45ff6d0
Resta <restasabrina22@gmail.com>
2025-03-18 23:07:30 +0700

    Tugas1_web

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/product', function () {
+    return "Product Page";
+});
+
+Route::get('/about', function () {
+    return "About Page";
+});
+
+Route::get('/contact', function () {
+    return "Contact Page";
+});
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
